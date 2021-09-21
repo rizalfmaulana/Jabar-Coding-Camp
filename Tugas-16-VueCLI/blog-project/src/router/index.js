5bbcb0acd18b519c062b76872316663a12071e4d
Rizal Fachrudin Maulana <rizalfmaulana@gmail.com>
2021-09-21 08:50:13 +0700

    Tugas-16-VueCLI

---
+++ Tugas-16-VueCLI/blog-project/src/router/index.js
@@ -0,0 +1,28 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: () => import(/* webpackChunkName: "about" */ "../views/Home.vue"),
+  },
+  {
+    path: "/blogs",
+    name: "Blogs",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ "../views/Blogs.vue"),
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
---
+++ Tugas-16-VueCLI/blog-project/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1>THis is Home page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    apiDomain: "http://demo-api-vue.sanbercloud.com",
+    blogs: [],
+  }),
+  created() {
+    const api = "http://demo-api-vue.sanbercloud.com/api/v2/blog/random/3";
+    this.axios.get(api).then((response) => console.log(response.data));
+  },
+};
+</script>
