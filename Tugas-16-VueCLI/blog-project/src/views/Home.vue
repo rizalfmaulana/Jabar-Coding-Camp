041524f44fb7f46e3460af8f4695278cd61a6901
Rizal Fachrudin Maulana <rizalfmaulana@gmail.com>
2021-09-21 09:52:03 +0700

    Tugas-16-VueCLI

--- Tugas-16-VueCLI/blog-project/src/views/Home.vue
+++ Tugas-16-VueCLI/blog-project/src/views/Home.vue
@@ -4,31 +4,22 @@
       <v-btn small text to="/blogs" class="blue--text"> All Blogs <v-icon>mdi-chevron-right</v-icon> </v-btn>
     </div>
     <v-layout wrap>
-      <v-flex v-for="blog in blogs" :key="`blog-${blog.id}`" xs6>
-        <v-card to="'/blog' + blog.id">
-          <v-img :src="blog.photo ? apiDomain + blog.photo : 'https://www.ilmubahasainggris.com/wp-content/uploads/2017/03/NGC.jpg'" class="white--text" height="200px">
-            <v-card-title class="fill-height align-end" v-text="blog.title"></v-card-title>
-          </v-img>
-
-          <v-card-actions>
-            <v-progress-linear color="blue-grey" height="7"></v-progress-linear>
-          </v-card-actions>
-
-          <v-card-actions>
-            <span>{{ blog.title.substring(0, 15) }}...</span>
-          </v-card-actions>
-        </v-card>
-      </v-flex>
+      <blog-item-component v-for="blog in blogs" :key="`blog-${blog.id}`" :blog="blog"></blog-item-component>
     </v-layout>
   </v-container>
 </template>
 
 <script>
+import BlogItemComponent from "../components/BlogItemComponent.vue";
 export default {
   data: () => ({
     apiDomain: "http://demo-api-vue.sanbercloud.com",
     blogs: [],
   }),
+
+  components: {
+    "blog-item-component": BlogItemComponent,
+  },
   created() {
     const config = {
       method: "get",
